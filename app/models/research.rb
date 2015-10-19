60f0822aae30e56b40b596b230b4bdfef9d668f1
Garrett Sand <garrettsand@gmail.com>
2015-10-19 16:29:30 -0700

    Fix resource model

--- app/models/research.rb
+++ app/models/research.rb
@@ -1,12 +1,6 @@
 class Research < ActiveRecord::Base
   belongs_to :job
   
-  field :company_mission, type: String
-  field :company_goals, type: String
-  field :current_projects, type: String
-  field :done, type: Mongoid::Boolean
-
-  # Functionality below seems unnecessary
-  # has_many :notes
-  # has_many :contacts
+  has_many :notes
+  has_many :contacts
 end
