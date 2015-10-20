00f80770126407ae7394718ac887a74a7d1f61e9
Mary Dickson Diaz <mdickson@gmail.com>
2015-10-20 09:25:34 -0700

    add tests for job resources

--- test/features/jobs/job_details_test.rb
+++ test/features/jobs/job_details_test.rb
@@ -1,31 +1,30 @@
 require 'test_helper'
 
-feature 'When a user visits their dashboard they can see all their jobs' do
-  scenario "users cannot see other users jobs" do
+feature 'User can view and edit job details' do
+  scenario "users can update job details" do
     visit root_path
     fill_in 'user_email', with: users(:mary).email
     fill_in 'user_password', with: "password"
     click_button('Log in')
     page.text.must_include('Cat Wrangler')
-    page.text.wont_include('Saxophone Developer')
+    click_button('Edit')
+    fill_in 'job title', with: 'Web Dev'
+    click_button('Save')
+    page.text.must_include('Job was successfully updated.')
+    page.text.must_include('Web Dev')
   end
 
-  scenario "should see all jobs on dashboard" do
+  scenario "can add a note to a job detail" do
     visit root_path
     fill_in 'user_email', with: users(:scott).email
     fill_in 'user_password', with: "password"
     click_button('Log in')
     page.text.must_include('Saxophone Developer')
-    page.text.must_include("Sweet Jams Developer")
-  end
-
-  scenario "Should be able to view job steps" do
-    visit root_path
-    fill_in 'user_email', with: users(:mary).email
-    fill_in 'user_password', with: "password"
-    click_button('Log in')
-    click_on("Cat Wrangler")
-    page.text.must_include('Job Steps')
+    click_button('Add Note')
+    fill_in 'content', with: "I really want this job"
+    click_button('Save')
+    page.text.must_include('Note was successfully added.')
+    page.text.must_include('I really want this job')
   end
 
   scenario "Should be able to view job details" do
