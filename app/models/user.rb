aa8676c9a6b1f02a6a92f6221dfd4e8a0d52a693
Garrett Sand <garrettsand@gmail.com>
2015-10-22 14:33:16 -0700

    Temporarily disable email confirmation to get app working on heroku again

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  protected
+  def confirmation_required?
+    false
+  end
 end
