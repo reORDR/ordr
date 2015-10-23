9a5a607bf61de301c0f73b2a9572817c00cf1a19
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-10-22 23:54:59 -0700

    Add helper methods for note edit/delete endpoints

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,13 @@ class Note < ActiveRecord::Base
   belongs_to :notable, polymorphic: true
 
   validates :body, presence: true
+
+  def notable_note_path
+    notable = Job::STEPS["#{ self.notable_type.underscore }"]
+    "/#{notable}/#{self.notable_id}/notes/#{self.id}"
+  end
+
+  def edit_notable_note_path
+    notable_note_path + "/edit"
+  end
 end
