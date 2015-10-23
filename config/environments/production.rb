2977706a9f1d8ec9e130d94beedeb75cbbf80108
Garrett Sand <garrettsand@gmail.com>
2015-10-23 10:19:17 -0700

    Change confirmation email url to our new domain ordr.es

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { :host => 'reordr.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'reordr.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => 'www.ordr.es' }
+  Rails.application.routes.default_url_options[:host] = 'www.ordr.es'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
