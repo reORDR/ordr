a6b1026bca7f1ab83343ff54dae780d16b11b765
Mary Dickson Diaz <mdickson@gmail.com>
2015-10-22 18:59:44 -0700

    commiting changes to switch branches

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_many :jobs, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
